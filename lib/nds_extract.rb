5a9ed81d562e40f932b6283701ab7f75f207499b
Rick Glascock <rixong@gmail.com>
2020-01-15 12:33:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,9 +35,6 @@ end
 
 def movies_with_director_key(name, movies_collection)
   
-  
-  pp directors_database
-  
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
