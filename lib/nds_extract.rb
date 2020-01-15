33562afe141721262cda38862fb3cc6898330bc8
Rick Glascock <rixong@gmail.com>
2020-01-15 13:49:18 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,6 @@ require 'directors_database'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
-
 def flatten_a_o_a(aoa)
   result = []
   i = 0
@@ -34,48 +33,32 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  
+  result = []
+  movies_collection.each { |movie|
+    result.push(movie_with_director_name(name, movie))
+  }
+  return result
 end
 
 
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+  studio_gross = {}
+  collection.each { |movie|
+    
+    studio_gross[movie[:studio]] ? studio_gross[movie[:studio]] += movie[:worldwide_gross] : 
+        studio_gross[movie[:studio]] = movie[:worldwide_gross]
+  }
+  return studio_gross
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+  
+  result = []
+  source.each { |director|
+    result.push(movies_with_director_key(director[:name], director[:movies]))
+  }
+  return result
 end
 
 # ----------------    End of Your Code Region --------------------
